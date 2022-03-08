16b7cc4c101a40b316e4e969b26261732cef2abb
Adam3018 <adam.adam3018@gmail.com>
2022-03-08 22:17:59 +0100

    SveFunkcije

--- src/main/java/com/opencsv/csvopen/UploadController.java
+++ src/main/java/com/opencsv/csvopen/UploadController.java
@@ -1,23 +1,22 @@
 package com.opencsv.csvopen;
 
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.Map;
+import java.text.SimpleDateFormat;
 
-import com.opencsv.CSVParser;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,11 +26,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
     public int rowNum;
     public int profitLoss;
-    public int expense1;
-    public int expense2;
-    public int prev;
-    public int prev1;
-    public int expense3;
     public int transNumJan=0;
     public int transNumFeb=0;
     public int transNumMar=0;
@@ -44,15 +38,8 @@ public class UploadController {
     public int transNumOct=0;
     public int transNumNov=0;
     public int transNumDec=0;
-    String ts;
-    String ts2;
-    String ts3;
-    
-    @GetMapping("/adam")
-
-    public String index(){
-        return "adam";
-    }
+    Map<String,List<Integer>> hashMapTransactions = new HashMap<>();
+    HashSet<Integer> hashTransactionValues = new HashSet<Integer>();
     
     @PostMapping("/upload")
     
@@ -60,64 +47,59 @@ public class UploadController {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
             String line="";
+            
             while((line = br.readLine()) != null){
                 String[] values = line.split(",");
+
+                if(hashMapTransactions.containsKey(values[2]))
+                hashMapTransactions.get(values[2]).add(Integer.parseInt(values[1]));
+                else
+                {
+                List<Integer> list = new ArrayList<>();
+                list.add(Integer.parseInt(values[1]));
+                hashMapTransactions.put(values[2], list);
+                }
+
+                hashTransactionValues.add(Integer.parseInt(values[1]));
+                
+
                 profitLoss= profitLoss+Integer.parseInt(values[1]);
-
-                int hlp=values[0].charAt(4);
-                if(hlp==49)
-                transNumJan++;
-                else if(hlp==50)
-                transNumFeb++;
-                else if(hlp==51)
-                transNumMar++;
-                else if(hlp==52)
-                transNumApr++;
-                else if(hlp==53)
-                transNumMay++;
-                else if(hlp==54)
-                transNumJun++;
-                else if(hlp==55)
-                transNumJul++;
-                else if(hlp==56)
-                transNumAvg++;
-                else if(hlp==57)
-                transNumSep++;
-                else if(hlp==58)
-                transNumOct++;
-                else if(hlp==59)
-                transNumNov++;
-                else if(hlp==60)
-                transNumDec++;
-
-
-                if(expense1>Integer.parseInt(values[1]))
-                {
-                expense1=Integer.parseInt(values[1]);
-                ts=expense1+" "+values[2];
-                }
-                if(expense2>Integer.parseInt(values[1]))
-                {
-                expense2=Integer.parseInt(values[1]);
-                ts2=expense2+" "+values[2];
-                }
-                if(expense3>Integer.parseInt(values[1]))
-                {
-                expense3=Integer.parseInt(values[1]);
-                ts3=expense3+" "+values[2];
-                }
-                if(expense2==expense1)
-                expense2=prev;
-                if(expense3==expense2 || expense3==expense1)
-                expense3=prev1;
                 
-                prev=expense2;
-                prev1=expense3;
+
+                Date simpleDate=new SimpleDateFormat("dd-MM-yyyy").parse(values[0]);
+                Calendar cal=new GregorianCalendar();
+                cal.setTime(simpleDate);
+
+                int month=cal.get(Calendar.MONTH)+1;
+
+                switch (month) {
+                    case 1:  transNumJan++;
+                             break;
+                    case 2:  transNumFeb++;
+                             break;
+                    case 3:  transNumMar++;
+                             break;
+                    case 4:  transNumApr++;
+                             break;
+                    case 5:  transNumMay++;
+                             break;
+                    case 6:  transNumJun++;
+                             break;
+                    case 7:  transNumJul++;
+                             break;
+                    case 8:  transNumAvg++;
+                             break;
+                    case 9:  transNumSep++;
+                             break;
+                    case 10: transNumOct++;
+                             break;
+                    case 11: transNumNov++;
+                             break;
+                    case 12: transNumDec++;
+                             break;
+                }
                 rowNum++;
-                
             }
-           
-           
         }
         catch(FileNotFoundException e){
             e.printStackTrace();
@@ -125,14 +107,16 @@ public class UploadController {
             e.printStackTrace();
         }
 
-        System.out.println(expense1+" "+expense2+" "+expense3);
+        List<Integer> listValues = new ArrayList<Integer>(hashTransactionValues);
+        Collections.sort(listValues);
+        
+        String s=hashMapTransactions.toString();
 
         String rtrn = "Number of rows: "+rowNum+
-        "\nProfit/loss: "+profitLoss+
-        "\nNum1 expense: "+ts+
-        "\nNum2 expense: "+ts2+
-        "\nNum3 expense: "+ts3+
-        "\nTransactions per month:\r\nJan "+transNumJan+
+        "\n\nTop 3 expenses: "+listValues.get(0)+"\n"+listValues.get(1)+"\n"+listValues.get(2)+
+        "\n\nExpense per category: "+s+
+        "\n\nProfit/loss: "+profitLoss+
+        "\n\nTransactions per month:\r\nJan "+transNumJan+
         "\nFeb "+transNumFeb+
         "\nMar "+transNumMar+
         "\nApr "+transNumApr+
@@ -146,37 +130,9 @@ public class UploadController {
         "\nDec "+transNumDec;
 
         return rtrn;
-    //     List<Transaction> transList = new ArrayList<>();
-    //     InputStream inputStream=file.getInputStream();
-    //    // CSVParserSettings setting=new CSVParserSettings();
-    //     CSVParser parser = new CSVParser();
-    //     List<Record> parseAll = ((Object) parser).parseAllRecords(inputStream);
-    //     return null;
-
-        // if (file.isEmpty()){
-        //     model.addAttribute("message","Please select a CSV file to upload.");
-        //     model.addAttribute("status", false);
-
-        // }
-        // else {
-        //     try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))){
-        //         CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder(reader)
-        //         .withType(Transaction.class)
-        //         .withIgnoreLeadingWhiteSpace(true)
-        //         .build();
-
-        //         List<Transaction> trns = csvToBean.parse();
-
-        //         model.addAttribute("transactions", trns);
-        //         model.addAttribute("status",true);
-        //     }
-        //     catch (Exception ex){
-        //         model.addAttribute("message","An error occured");
-        //         model.addAttribute("status",false);
-        //     }
-        // }
-        //return rowNum;
         
     }
+
+   
     
 }
